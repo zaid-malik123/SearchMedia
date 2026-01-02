1e53735a25d1eba8e8a4cbe62151dadb5ab110d3
zaidmalik <mzaidmalik25@gmail.com>
2026-01-02 21:35:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { searchGIF, searchPhotos, searchVideos } from './api/mediaApi'
+
+const App = () => {
+
+  const SearchVideos = async () => {
+    const data = await searchVideos("cat")
+    console.log(data)
+  }
+  const SearchPhotos = async () => {
+    const data = await searchPhotos("cat")
+    console.log(data)
+  }
+
+
+  const SearchGIF = async () => {
+    const data = await searchGIF("cat")
+    console.log(data)
+  }
+
+  return (
+    <div className=''>
+      
+      <button onClick={SearchPhotos} className='px-5 py-2 bg-black text-white rounded-md ml-10'>Photos</button>
+      <button onClick={SearchVideos} className='px-5 py-2 bg-black text-white rounded-md ml-10'>Videos</button>
+      <button onClick={SearchGIF} className='px-5 py-2 bg-black text-white rounded-md ml-10'>GIF</button>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/mediaApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+const UNSPLASH_API = import.meta.env.VITE_UNSPALSH_KEY
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
+const TENOR_KEY = import.meta.env.VITE_TENOR_KEY
+
+
+export async function searchPhotos(query, page=1, per_page=20) {
+    const res = await axios.get("https://api.unsplash.com/photos", {
+        params: {query, page, per_page },
+
+        headers: {Authorization: `Client-ID ${UNSPLASH_API}`}
+    })
+    return res.data
+}
+
+export async function searchVideos(query, per_page=20) {
+    const res = await axios.get("https://api.pexels.com/videos/search", {
+        params: {query, per_page },
+        headers: {Authorization: PEXELS_KEY}
+    })
+    return res.data.videos
+}
+
+export async function searchGIF(query, limit=20) {
+    const res = await axios.get("https://tenor.googleapis.com/v2/search", {
+        params: {q:query, key:TENOR_KEY, limit },
+        
+    })
+    return res.data.results
+}
